a724f4e73376e2a2867411985e667782f40ff8bd
alex <am3081@gmail.com>
2019-01-16 15:47:25 -0500

    updated flood data

--- tasks/irvs/load_ebr_floodplain.py
+++ tasks/irvs/load_ebr_floodplain.py
@@ -6,26 +6,32 @@ def loadData(cursor):
 	sql = '''
 		PREPARE stmt AS
 		UPDATE irvs.enhanced_building_risk
-		SET floodplain = $1
-		WHERE building_id = $2
+		SET 
+			flood_zone = $1,
+			flood_depth = $2,
+			flood_velocity=$3,
+			flood_base_elevation=$4
+		WHERE building_id = $5
 	'''
 	cursor.execute(sql)
 
 	sql = '''
-		SELECT string_agg("FLD_ZONE", '|'), building_id
+		SELECT array_agg("FLD_ZONE") as flood_zone, max("DEPTH") as flood_depth, max("VELOCITY") as flood_velocity, max("STATIC_BFE") as base_flood_elevation, building_id
 		FROM irvs.buildings
-		INNER JOIN "flood_DFIRM"."nys_flood_DFIRM"
-		ON ST_Intersects(ST_Transform(geom, 4326), footprint)
+		INNER JOIN "flood_DFIRM"."nys_flood_DFIRM_4326"
+		ON ST_Intersects(geom, footprint)
 		WHERE footprint IS NOT NULL
+		AND cousub_geoid = '3600101000'
 		AND building_id IN (
 			SELECT building_id
 			FROM irvs.enhanced_building_risk
 		)
-		GROUP BY building_id
+		GROUP BY building_id 
+		limit 100
 	'''
 	cursor.execute(sql)
 	for row in [r for r in cursor]:
-		cursor.execute("EXECUTE stmt(%s, %s)", row)
+		cursor.execute("EXECUTE stmt(%s, %s, %s, %s, %s)", row)
 
 	cursor.execute("DEALLOCATE stmt")
 
