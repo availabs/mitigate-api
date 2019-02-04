8260d727da9c3df2cd8f4a6b30f3791484a25b5f
nayanikabhargava <nayanikabhargava1993@gmail.com>
2019-02-04 10:08:48 -0500

    census Config file

--- services/utils/censusConfig.js
+++ services/utils/censusConfig.js
@@ -1,8 +1,564 @@
 module.exports = {
     B01003: {
         name: 'Overall Population',
-        subvariables: [
-            {name: 'Total Population', key: 'B01003_001E'}
+        variables: [
+            'B01003_001E'
+        ]
+    },
+    B02001: {
+        name: 'Racial Population',
+        variables: [
+            'B02001_001E',
+            'B02001_002E',
+            'B02001_003E',
+            'B02001_004E',
+            'B02001_005E',
+            'B02001_006E',
+            'B02001_007E',
+            'B02001_008E',
+            'B02001_009E',
+            'B02001_010E'
+        ]
+    },
+    B01002: {
+        name: 'Median Age by Sex',
+        variables: [
+            'B01002_001E',
+            'B01002_002E',
+            'B01002_003E'
+        ]
+    },
+
+}
+
+/*
+    B19057:{
+        name: 'Public Assistance Income for Households',
+        variables:[
+            'B19057_001E',
+            'B19057_002E',
+            'B19057_003E'
+        ]
+    },
+    B23025: {
+        name: 'Employment Status',
+        variables:[
+            'B23025_001E',
+            'B23025_002E',
+            'B23025_003E',
+            'B23025_004E',
+            'B23025_005E',
+            'B23025_006E',
+            'B23025_007E'
+        ]
+    },
+    C02003:{
+        name: 'Ethinic Population by Race',
+        variables:[
+            'C02003_001E',
+            'C02003_002E',
+            'C02003_003E',
+            'C02003_004E',
+            'C02003_005E',
+            'C02003_006E',
+            'C02003_007E',
+            'C02003_008E',
+            'C02003_009E',
+            'C02003_010E',
+            'C02003_011E',
+            'C02003_012E'
+        ]
+    },
+    B19083:{
+        name:'Gini Index of Income Inequality',
+        variables:[
+            'B19083_001E'
+        ]
+    },
+    B19013:{
+        name:'Median Household Income',
+        variables:[
+            'B19013_001E'
+        ]
+    },
+    B19025:{
+        name:'Aggregate Household Income',
+        variables:[
+            'B19025_001E'
+        ]
+    },
+    B19301:{
+        name:'Per Capita Income',
+        variables:[
+            'B19301_001E'
+        ]
+    },
+    B23020:{
+        name:'Mean Usual Hours Worked for Workers 16 to 64 Years',
+        variables:[
+            'B23020_001E',
+            'B23020_002E',
+            'B23020_003E'
+        ]
+    },
+    B25003:{
+        name: 'Occupied Housing Units Tenure',
+        variables:[
+            'B25003_001E',
+            'B25003_002E',
+            'B25003_003E'
+        ]
+    },
+    B08604:{
+        name: 'Worker Population',
+        variables: [
+            'B08604_001E'
+        ]
+    },
+    B15003:{
+        name: 'Educational Attainment',
+        variables:[
+            'B15003_001E'
+        ]
+    },
+    B08301:{
+        name: 'Means of Transportation to Work',
+        variables:[
+            'B08301_001E',
+            'B08301_002E',
+            'B08301_003E',
+            'B08301_004E',
+            'B08301_005E',
+            'B08301_006E',
+            'B08301_007E',
+            'B08301_008E',
+            'B08301_009E',
+            'B08301_010E',
+            'B08301_011E',
+            'B08301_012E',
+            'B08301_013E',
+            'B08301_014E',
+            'B08301_015E',
+            'B08301_016E',
+            'B08301_017E',
+            'B08301_018E',
+            'B08301_019E',
+            'B08301_020E',
+            'B08301_021E'
+        ]
+    },
+    B08303:{
+        name: 'Travel Time to Work',
+        variables:[
+            'B08303_001E',
+            'B08303_002E',
+            'B08303_003E',
+            'B08303_004E',
+            'B08303_005E',
+            'B08303_006E',
+            'B08303_007E',
+            'B08303_008E',
+            'B08303_009E',
+            'B08303_010E',
+            'B08303_011E',
+            'B08303_012E',
+            'B08303_013E'
+        ]
+    },
+
+    B25077:{
+        name: 'Housing Units Median Value',
+        variables:[
+            'B25077_001E'
+        ]
+    },
+    B17001:{
+        name: 'Poverty Status in the Past 12 Months',
+        variables:[
+            'B17001_001E',
+            'B17001_002E',
+            'B17001_003E',
+            'B17001_004E',
+            'B17001_005E',
+            'B17001_006E',
+            'B17001_007E',
+            'B17001_008E',
+            'B17001_009E',
+            'B17001_010E',
+            'B17001_011E',
+            'B17001_012E',
+            'B17001_013E',
+            'B17001_014E',
+            'B17001_015E',
+            'B17001_016E',
+            'B17001_017E',
+            'B17001_018E',
+            'B17001_019E',
+            'B17001_020E',
+            'B17001_021E',
+            'B17001_022E',
+            'B17001_023E',
+            'B17001_024E',
+            'B17001_025E',
+            'B17001_026E',
+            'B17001_027E',
+            'B17001_028E',
+            'B17001_029E',
+            'B17001_030E',
+            'B17001_031E',
+            'B17001_032E',
+            'B17001_033E',
+            'B17001_034E',
+            'B17001_035E',
+            'B17001_036E',
+            'B17001_037E',
+            'B17001_038E',
+            'B17001_039E',
+            'B17001_040E',
+            'B17001_041E',
+            'B17001_042E',
+            'B17001_043E',
+            'B17001_044E',
+            'B17001_045E',
+            'B17001_046E',
+            'B17001_047E',
+            'B17001_048E',
+            'B17001_049E',
+            'B17001_050E',
+            'B17001_051E',
+            'B17001_052E',
+            'B17001_053E',
+            'B17001_054E',
+            'B17001_055E',
+            'B17001_056E',
+            'B17001_057E',
+            'B17001_058E',
+            'B17001_059E'
+        ]
+    },
+    B11001:{
+        name: 'Population by household type',
+        variables:[
+            'B11001_001E',
+            'B11001_002E',
+            'B11001_003E',
+            'B11001_004E',
+            'B11001_005E',
+            'B11001_006E',
+            'B11001_007E',
+            'B11001_008E',
+            'B11001_009E'
+        ]
+    },
+    B25002:{
+        name: 'Occupied vs. Vacant Housing Units',
+        variables:[
+            'B25002_001E',
+            'B25002_002E',
+            'B25002_003E'
+        ]
+    },
+    B21002:{
+        name: 'Veterans by wartime service',
+        variables:[
+            'B21002_001E',
+            'B21002_002E',
+            'B21002_003E',
+            'B21002_004E',
+            'B21002_005E',
+            'B21002_006E',
+            'B21002_007E',
+            'B21002_008E',
+            'B21002_009E',
+            'B21002_010E',
+            'B21002_011E',
+            'B21002_012E',
+            'B21002_013E',
+            'B21002_014E',
+            'B21002_015E',
+            'B21002_016E'
+        ]
+    },
+    B22005A:{
+        name: 'Receipt of Food Stamps/SNAP by Race (White)',
+        variables:[
+            'B22005A_001E',
+            'B22005A_002E',
+            'B22005A_003E'
+        ]
+
+    },
+    B22005B:{
+        name: 'Receipt of Food Stamps/SNAP by Race (Black or African American Alone)',
+        variables:[
+            'B22005B_001E',
+            'B22005B_002E',
+            'B22005B_003E'
+        ]
+    },
+    B22005C:{
+        name: 'Receipt of Food Stamps/SNAP by Race (American Indian and Alaska Native)',
+        variables:[
+            'B22005C_001E',
+            'B22005C_002E',
+            'B22005C_003E'
+        ]
+    },
+    B22005D:{
+        name:'Receipt of Food Stamps/SNAP by Race (Asian)',
+        variables:[
+            'B22005D_001E',
+            'B22005D_002E',
+            'B22005D_003E'
+        ]
+    },
+    B22005E:{
+        name: 'Receipt of Food Stamps/SNAP by Race (Native Hawaiian and Other Pacific Islander)',
+        variables:[
+            'B22005E_001E',
+            'B22005E_002E',
+            'B22005E_003E'
+        ]
+    },
+    B22005F:{
+        name: 'Receipt of Food Stamps/SNAP by Race (Some Other Race Alone)',
+        variables:[
+            'B22005F_001E',
+            'B22005F_002E',
+            'B22005F_003E'
+        ]
+    },
+    B22005G:{
+        name: 'Receipt of Food Stamps/SNAP by Race (Two or More Races)',
+        variables:[
+            'B22005G_001E',
+            'B22005G_002E',
+            'B22005G_003E'
+        ]
+    },
+    B22005H:{
+        name: 'Receipt of Food Stamps/SNAP by Race (White Alone, Not Hispanic or Latino)',
+        variables:[
+            'B22005H_001E',
+            'B22005H_002E',
+            'B22005H_003E'
+        ]
+    },
+    B22005I:{
+        name: 'Receipt of Food Stamps/SNAP by Race (Hispanic or Latino)',
+        variables:[
+            'B22005I_001E',
+            'B22005I_002E',
+            'B22005I_003E'
+        ]
+    },
+    B05001:{
+        name: 'Citizenship status',
+        variables:[
+            'B05001_001E',
+            'B05001_002E',
+            'B05001_003E',
+            'B05001_004E',
+            'B05001_005E',
+            'B05001_006E'
+        ]
+    },
+    B19013A:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013A_001E'
+        ]
+    },
+    B19013B:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013B_001E'
+        ]
+    },
+    B19013C:{
+        name: 'Median Household Income by Race',
+        variable:[
+            'B19013C_001E'
+        ]
+    },
+    B19013D:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013D_001E'
+        ]
+    },
+    B19013E:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013E_001E'
+        ]
+    },
+    B19013F:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013F_001E'
+        ]
+    },
+    B19013H:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013H_001E'
+        ]
+    },
+    B19013I:{
+        name: 'Median Household Income by Race',
+        variables:[
+            'B19013I_001E'
+        ]
+    },
+    B05001:{
+        name: 'Immigrant/Refugee indicators',
+        variables:[
+            'B05001_001E',
+            'B05001_002E',
+            'B05001_003E',
+            'B05001_004E',
+            'B05001_005E',
+            'B05001_006E',
+        ]
+    },
+    B25125:{
+        name: 'Tenure by Age of Structure',
+        variables:[
+            'B25125_001E',
+            'B25125_002E',
+            'B25125_003E',
+            'B25125_004E',
+            'B25125_005E',
+            'B25125_006E',
+            'B25125_007E',
+            'B25125_008E',
+            'B25125_009E',
+            'B25125_010E',
+            'B25125_011E',
+            'B25125_012E',
+            'B25125_013E',
+            'B25125_014E',
+            'B25125_015E',
+            'B25125_016E',
+            'B25125_017E',
+            'B25125_018E',
+            'B25125_019E',
+            'B25125_020E',
+            'B25125_021E',
+            'B25125_022E',
+            'B25125_023E',
+            'B25125_024E',
+            'B25125_025E',
+            'B25125_026E',
+            'B25125_027E',
+            'B25125_028E',
+            'B25125_029E',
+            'B25125_030E',
+            'B25125_032E',
+            'B25125_033E',
+            'B25125_034E',
+            'B25125_035E',
+            'B25125_036E',
+            'B25125_037E',
+            'B25125_038E',
+            'B25125_039E',
+            'B25125_040E',
+            'B25125_041E',
+            'B25125_042E',
+            'B25125_043E',
+            'B25125_044E',
+            'B25125_045E'
+        ]
+    },
+    B25102:{
+        name: 'Mortgage Status by Real Estate Taxes Paid',
+        variables:[
+            'B25102_001E',
+            'B25102_002E',
+            'B25102_003E',
+            'B25102_004E',
+            'B25102_005E',
+            'B25102_006E',
+            'B25102_007E',
+            'B25102_008E',
+            'B25102_009E',
+            'B25102_010E',
+            'B25102_011E',
+            'B25102_012E',
+            'B25102_013E',
+            'B25102_014E',
+            'B25102_015E'
+        ]
+    },
+    B25002:{
+        name: 'Occupancy Status',
+        variables:[
+            'B25002_001E',
+            'B25002_002E',
+            'B25002_003E'
+        ]
+    },
+    B15003:{
+        name: 'Educational Attainment among adults 25 years and older',
+        variables:[
+            'B15003_001E',
+            'B15003_002E',
+            'B15003_003E',
+            'B15003_004E',
+            'B15003_005E',
+            'B15003_006E',
+            'B15003_007E',
+            'B15003_008E',
+            'B15003_009E',
+            'B15003_010E',
+            'B15003_011E',
+            'B15003_012E',
+            'B15003_013E',
+            'B15003_014E',
+            'B15003_015E',
+            'B15003_016E',
+            'B15003_017E',
+            'B15003_018E',
+            'B15003_019E',
+            'B15003_020E',
+            'B15003_021E',
+            'B15003_022E',
+            'B15003_023E',
+            'B15003_024E',
+            'B15003_025E'
+        ]
+    },
+    B21002:{
+        name: 'Veterans Population',
+        variables:[
+            'B21002_001E',
+            'B21002_002E',
+            'B21002_003E',
+            'B21002_004E',
+            'B21002_005E',
+            'B21002_006E',
+            'B21002_007E',
+            'B21002_008E',
+            'B21002_009E',
+            'B21002_010E',
+            'B21002_011E',
+            'B21002_012E',
+            'B21002_013E',
+            'B21002_014E',
+            'B21002_015E',
+            'B21002_016E'
         ]
     }
-}
+
+
+
+
+
+    B25075:{
+        name: 'Housing Units Value by Bucket',
+        variables:[
+            //These have 27 variables mentioned in the excel but 25 on the acs census list
+        ]
+    },
+     */
