ccf0048d9862b8cc56ef62eedba561498818332f
nayanikabhargava <nayanikabhargava1993@gmail.com>
2019-03-28 13:58:08 -0400

    added variables for Sex by age, language, children

--- services/utils/censusConfig.js
+++ services/utils/censusConfig.js
@@ -1,4 +1,146 @@
 module.exports = {
+    B01001: { // Added a new one for graphs
+        name: 'Sex by Age',
+        variables: [
+            {name:'Total Population',value:'B01001_001E'},
+            {name:'Male Population',value:'B01001_002E'},
+            {name:'Male Under 5',value:'B01001_003E'},
+            {name:'Male 5-9',value:'B01001_004E'},
+            {name:'Male 10-14',value:'B01001_005E'},
+            {name:'Male 15-17',value:'B01001_006E'},
+            {name:'Male 18-19',value:'B01001_007E'},
+            {name:'Male 20',value:'B01001_008E'},
+            {name:'Male 21',value:'B01001_009E'},
+            {name:'Male 22-24',value:'B01001_010E'},
+            {name:'Male 25-29',value:'B01001_011E'},
+            {name:'Male 30-34',value:'B01001_012E'},
+            {name:'Male 35-39',value:'B01001_013E'},
+            {name:'Male 40-44',value:'B01001_014E'},
+            {name:'Male 45-49',value:'B01001_015E'},
+            {name:'Male 50-54',value:'B01001_016E'},
+            {name:'Male 55-59',value:'B01001_017E'},
+            {name:'Male 60-61',value:'B01001_018E'},
+            {name:'Male 62-64',value:'B01001_019E'},
+            {name:'Male 65-66',value:'B01001_020E'},
+            {name:'Male 67-69',value:'B01001_021E'},
+            {name:'Male 70-74',value:'B01001_022E'},
+            {name:'Male 75-79',value:'B01001_023E'},
+            {name:'Male 80-84',value:'B01001_024E'},
+            {name:'Male 85 over',value:'B01001_025E'},
+            //-------------- starts------------------
+            {name:'Female Population',value:'B01001_026E'},
+            {name:'Female Under 5',value:'B01001_027E'},
+            {name:'Female 5-9',value:'B01001_028E'},
+            {name:'Female 10-14',value:'B01001_029E'},
+            {name:'Female 15-17',value:'B01001_030E'},
+            {name:'Female 18-19',value:'B01001_031E'},
+            {name:'Female 20',value:'B01001_032E'},
+            {name:'Female 21',value:'B01001_033E'},
+            {name:'Female 22-24',value:'B01001_034E'},
+            {name:'Female 25-29',value:'B01001_035E'},
+            {name:'Female 30-34',value:'B01001_036E'},
+            {name:'Female 35-39',value:'B01001_037E'},
+            {name:'Female 40-44',value:'B01001_038E'},
+            {name:'Female 45-49',value:'B01001_039E'},
+            {name:'Female 50-54',value:'B01001_040E'},
+            {name:'Female 55-59',value:'B01001_041E'},
+            {name:'Female 60-61',value:'B01001_042E'},
+            {name:'Female 62-64',value:'B01001_043E'},
+            {name:'Female 65-66',value:'B01001_044E'},
+            {name:'Female 67-69',value:'B01001_045E'},
+            {name:'Female 70-74',value:'B01001_046E'},
+            {name:'Female 75-79',value:'B01001_047E'},
+            {name:'Female 80-84',value:'B01001_048E'},
+            {name:'Female 85 over',value:'B01001_049E'}
+
+        ]
+    },
+    B16001: {
+      name: 'Language Spoken at Home by Ability to Speak English',
+      variables : [
+          {name:'Spanish or Spanish Creole Speak English very well',value:'B16001_004E'},
+          {name:'Spanish or Spanish Creole Speak English Less than very well',value:'B16001_005E'},
+          {name:'French (incl. Patois,Cajun) Speak English very well',value:'B16001_007E'},
+          {name:'French (incl. Patois,Cajun) Speak English Less than very well',value:'B16001_008E'},
+          {name:'French Creole Speak English very well',value:'B16001_010E'},
+          {name:'French Creole Speak English Less than very well',value:'B16001_011E'},
+          {name:'Italian Speak English very well',value:'B16001_013E'},
+          {name:'Italian Speak English Less than very well',value:'B16001_014E'},
+          {name:'Portuguese or Portuguese Creole Speak English very well',value:'B16001_016E'},
+          {name:'Portuguese or Portuguese Creole Speak English Less than very well',value:'B16001_017E'},
+          {name:'German Speak English very well',value:'B16001_019E'},
+          {name:'German Speak English Less than very well',value:'B16001_020E'},
+          {name:'Yiddish Speak English very well',value:'B16001_022E'},
+          {name:'Yiddish Speak English Less than very well',value:'B16001_023E'},
+          {name:'Tagalog Speak English very well ',value:'B16001_094E'},
+          {name:'Tagalog Speak English Less than very well ',value:'B16001_095E'},
+          {name:'Greek Speak English very well',value:'B16001_031E'},
+          {name:'Greek Speak English Less than very well',value:'B16001_031E'},
+          {name:'Russian Speak English very well',value:'B16001_034E'},
+          {name:'Russian Speak English Less than very well',value:'B16001_035E'},
+          {name:'Polish Speak English very well',value:'B16001_037E'},
+          {name:'Polish Speak English Less than very well',value:'B16001_038E'},
+          {name:'Serbo-Croatian Speak English very well',value:'B16001_040E'},
+          {name:'Serbo-Croatian Speak English Less than very well',value:'B16001_041E'},
+          {name:'Other Salvic Languages Speak English very well',value:'B16001_043E'},
+          {name:'Other Salvic Languages Speak English Less than very well',value:'B16001_044E'},
+          {name:'Other Asian Languages Speak English very well ',value:'B16001_091E'},
+          {name:'Other Asian Languages Speak English Less than very well ',value:'B16001_092E'},
+          {name:'Persian Speak English very well',value:'B16001_049E'},
+          {name:'Persian Speak English Less than very well',value:'B16001_050E'},
+          {name:'Gujarati Speak English very well',value:'B16001_052E'},
+          {name:'Gujarati Speak English Less than very well',value:'B16001_053E'},
+          {name:'Hindi Speak English very well',value:'B16001_055E'},
+          {name:'Hindi Speak English Less than very well',value:'B16001_056E'},
+          {name:'Urdu Speak English very well',value:'B16001_058E'},
+          {name:'Urdu Speak English Less than very well',value:'B16001_059E'},
+          {name:'Other Indic languages Speak English very well',value:'B16001_061E'},
+          {name:'Other Indic languages Speak English Less than very well',value:'B16001_062E'},
+          {name:'Other Indo-European languages Speak English very well',value:'B16001_064E'},
+          {name:'Other Indo-European languages Speak English Less than very well',value:'B16001_065E'},
+          {name:'Chinese Speak English very well',value:'B16001_067E'},
+          {name:'Chinese Speak English Less than very well',value:'B16001_068E'},
+          {name:'Japanese Speak English very well',value:'B16001_070E'},
+          {name:'Japanese Speak English Less than very well',value:'B16001_071E'},
+          {name:'Korean Speak English very well',value:'B16001_073E'},
+          {name:'Korean Speak English Less than very well',value:'B16001_074E'},
+          {name:'Mon-Khmer Cambodian Speak English very well',value:'B16001_076E'},
+          {name:'Mon-Khmer Cambodian Speak English Less than very well',value:'B16001_077E'},
+      ]
+    },
+    B23008: {
+        name: 'Own children under 18 years in families and subfamilies',
+        variables : [
+            {name: 'Total',value:'B23008_001E'}, // 0
+            {name: 'Under 6 years',value:'B23008_002E'},
+            {name: 'Living with two parents',value:'B23008_003E'}, //2
+            {name: 'Both Parents in labor force',value:'B23008_004E'},
+            {name: 'Father only in labor force',value:'B23008_005E'},
+            {name: 'Mother only in labor force',value:'B23008_006E'},
+            {name: 'Neither parent in labor force',value:'B23008_007E'},
+            {name: 'Living with one parent',value:'B23008_008E'},
+            {name: 'Living with father',value:'B23008_009E'}, // 8
+            {name: 'In labor force',value:'B23008_010E'},
+            {name: 'Not in labor force',value:'B23008_011E'},
+            {name: 'Living with mother',value:'B23008_012E'}, // 11
+            {name: 'In labor force',value:'B23008_013E'},
+            {name: 'Not in labor force',value:'B23008_014E'},
+            {name: '6 to 17 years',value:'B23008_015E'},
+            {name: 'Living with two parents',value:'B23008_016E'}, //15
+            {name: 'Both Parents in labor force',value:'B23008_017E'},
+            {name: 'Father only in labor force',value:'B23008_018E'},
+            {name: 'Mother only in labor force',value:'B23008_019E'},
+            {name: 'Neither parent in labor force',value:'B23008_020E'},
+            {name: 'Living with one parent',value:'B23008_021E'},
+            {name: 'Living with father',value:'B23008_022E'}, // 21
+            {name: 'In labor force',value:'B23008_023E'},
+            {name: 'Not in labor force',value:'B23008_024E'},
+            {name: 'Living with mother',value:'B23008_025E'}, // 24
+            {name: 'In labor force',value:'B23008_026E'},
+            {name: 'Not in labor force',value:'B23008_027E'},
+
+        ]
+    },
     B01003: {
         name: 'Overall Population',
         variables: [
@@ -13,44 +155,46 @@ module.exports = {
             {name:'D',value:'B01002_003E'}
         ]
     },
+
     B02001: {
         name: 'Racial Population',
         variables: [
             {name:'E',value:'B02001_001E'},
             {name:'F',value:'B02001_002E'},
-            {name:'D',value:'B02001_003E'},
-            {name:'G',value:'B02001_004E'},
-            {name:'H',value:'B02001_005E'},
-            {name:'I',value:'B02001_006E'},
-            {name:'J',value:'B02001_007E'},
-            {name:'K',value:'B02001_008E'},
-            {name:'L',value:'B02001_009E'},
-            {name:'M',value:'B02001_010E'}
+            {name:'G',value:'B02001_003E'},
+            {name:'H',value:'B02001_004E'},
+            {name:'I',value:'B02001_005E'},
+            {name:'J',value:'B02001_006E'},
+            {name:'K',value:'B02001_007E'},
+            {name:'L',value:'B02001_008E'},
+            {name:'M',value:'B02001_009E'},
+            {name:'N',value:'B02001_010E'}
         ]
     },
     B19057:{
         name: 'Public Assistance Income for Households',
         variables:[
-            {name:'N',value:'B19057_001E'},
-            {name:'O',value:'B19057_002E'},
-            {name:'P',value:'B19057_003E'}
+            {name:'O',value:'B19057_001E'},
+            {name:'P',value:'B19057_002E'},
+            {name:'Q',value:'B19057_003E'}
         ]
     },
     B23025: {
         name: 'Employment Status',
         variables:[
-            {name:'Q',value:'B23025_001E'},
-            {name:'R',value:'B23025_002E'},
-            {name:'S',value:'B23025_003E'},
-            {name:'T',value:'B23025_004E'},
-            {name:'U',value:'B23025_005E'},
-            {name:'V',value:'B23025_006E'},
-            {name:'W',value:'B23025_007E'}
+            {name:'R',value:'B23025_001E'},
+            {name:'S',value:'B23025_002E'},
+            {name:'T',value:'B23025_003E'},
+            {name:'U',value:'B23025_004E'},
+            {name:'V',value:'B23025_005E'},
+            {name:'W',value:'B23025_006E'},
+            {name:'X',value:'B23025_007E'}
         ]
     }
 
 }
 /*
+
     ,
     C02003:{
         name: 'Ethinic Population by Race',
@@ -94,7 +238,7 @@ module.exports = {
         ]
     },
     B23020:{
-        name:'Mean Usual Hours Worked for Workers 16 to 64 Years',
+        name:'Mean Usual Hours Worked for Workers 16 to 64 ',
         variables:[
             'B23020_001E',
             'B23020_002E',
@@ -500,7 +644,7 @@ module.exports = {
         ]
     },
     B15003:{
-        name: 'Educational Attainment among adults 25 years and older',
+        name: 'Educational Attainment among adults 25  and older',
         variables:[
             'B15003_001E',
             'B15003_002E',
@@ -561,4 +705,30 @@ module.exports = {
             //These have 27 variables mentioned in the excel but 25 on the acs census list
         ]
     },
+     ------------------------For the variable B16001 ---------------------------------------
+
+          {name:'Scandinavian Languages Speak English very well',value:'B16001_028E'},
+          {name:'Scandinavian Languages Speak English Less than very well',value:'B16001_029E'},
+          {name:'Thai Speak English very well ',value:'B16001_082E'},
+          {name:'Thai Speak English Less than very well ',value:'B16001_083E'},
+          {name:'Loatian Speak English very well ',value:'B16001_085E'},
+          {name:'Loatian Speak English Less than very well ',value:'B16001_086E'},
+          {name:'Vietnamese Speak English very well ',value:'B16001_088E'},
+          {name:'Vietnamese Speak English Less than very well ',value:'B16001_089E'},
+          {name:'Armenian Speak English very well',value:'B16001_046E'},
+          {name:'Armenian Speak English Less than very well',value:'B16001_047E'},
+          {name:'Hmong Speak English very well ',value:'B16001_079E'},
+          {name:'Hmong Speak English Less than very well ',value:'B16001_080E'},
+          {name:'Other Pacific Island Languages Speak English very well ',value:'B16001_097E'},
+          {name:'Other Pacific Island Languages Speak English Less than very well ',value:'B16001_098E'},
+          {name:'Other Native North American Languages Speak English very well ',value:'B16001_103E'},
+          {name:'Other Native North American Languages Speak English Less than very well ',value:'B16001_104E'},
+          {name:'Hungarian Speak English very well ',value:'B16001_106E'},
+          {name:'Hungarian Speak English Less than very well ',value:'B16001_107E'},
+          {name:'Arabic Speak English very well ',value:'B16001_109E'},
+          {name:'Arabic Speak English Less than very well ',value:'B16001_110E'},
+          {name:'Hebrew Speak English very well ',value:'B16001_112E'},
+          {name:'Hebrew Speak English Less than very well ',value:'B16001_113E'},
+          {name:'African Languages Speak English very well ',value:'B16001_115E'},
+          {name:'African Languages Speak English Less than very well ',value:'B16001_116E'}
      */
