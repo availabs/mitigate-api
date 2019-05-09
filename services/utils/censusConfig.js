ffec7944120a176bc93c5c711ead7f16d93bffce
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-09 12:00:38 -0400

    additions

--- services/utils/censusConfig.js
+++ services/utils/censusConfig.js
@@ -189,9 +189,52 @@ module.exports = {
 
         ]
     },
-
-
-
+    B25002:{
+        name:'Occupancy Status',
+        variables: [
+            {name:'Total',value:'B25002_001E'},
+            {name:'Occupied',value:'B25002_002E'},
+            {name:'Vacant',value:'B25002_003E'}
+        ]
+    },
+    B17001:{
+        name:'Poverty Status by Sex by Age',
+        variables:[
+            {name:'Income in the past 12 months below poverty level',value:'B17001_001E'}
+        ]
+    },
+    B15003:{
+        name:'Educational Attainment',
+        variables:[
+            /*
+            {name:'Total',value:'B15003_001E'},
+            {name:'No schooling completed',value:'B15003_002E'},
+            {name:'Nursery School',value:'B15003_003E'},
+            {name:'Kindergarten',value:'B15003_004E'},
+            {name:'1st grade',value:'B15003_005E'},
+            {name:'2nd grade',value:'B15003_006E'},
+            {name:'3rd grade',value:'B15003_007E'},
+            {name:'4th grade',value:'B15003_008E'},
+            {name:'5th grade',value:'B15003_009E'},
+            {name:'6th grade',value:'B15003_010E'},
+            {name:'7th grade',value:'B15003_011E'},
+            {name:'8th grade',value:'B15003_012E'},
+            {name:'9th grade',value:'B15003_013E'},
+            {name:'10th grade',value:'B15003_014E'},
+            {name:'11th grade',value:'B15003_015E'},
+            {name:'12th grade or no diploma',value:'B15003_016E'},
+             */
+            {name:'Regular High School diploma',value:'B15003_017E'},
+            {name:'GED or alternative credential',value:'B15003_018E'},
+            {name:'Some college less than 1 year',value:'B15003_019E'},
+            {name:'Some college,1 or more years,no degree',value:'B15003_020E'},
+            {name:'Associate`s degree',value:'B15003_021E'},
+            {name:'Bachelor`s degree',value:'B15003_022E'},
+            {name:'Master`s degree',value:'B15003_023E'},
+            {name:'Professional School degree',value:'B15003_024E'},
+            {name:'Doctorate degree',value:'B15003_025E'}
+        ]
+    }
 }
 /*
 
